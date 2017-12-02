ca6e88c6418701814d7b91fbd803af484bc0ce1d
Chris Holtz <choltz@gmail.com>
2017-12-02 10:09:40 -0500

    Properly(?) hooks single file vue templates in

--- app/initialize.js
+++ app/initialize.js
@@ -1,7 +1,20 @@
+import Vue from 'vue'
 import buh from 'javascripts/buh';
+import App from 'javascripts/app.vue';
+
+// Vue.config.productionTip = false
+
 
 document.addEventListener('DOMContentLoaded', function() {
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    template: '<App/>',
+    // components: { App },
+    render: h => h(App)
+  })
+
   // do your setup here
   console.log('Initialized app');
-  console.log(buh);
+  // console.log(test);
 });
