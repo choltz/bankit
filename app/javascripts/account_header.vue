ca6d03688f6512e7c6d58283606b07fc54714652
Chris Holtz <choltz@gmail.com>
2017-12-07 00:03:46 -0500

    Added account header component

---
+++ app/javascripts/account_header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="account-header">
+    All Accounts
+  </div>
+</template>
+
+<style>
+  .account-header {
+    color:            white;
+    background-color: #003540;
+
+    padding: .5em;
+    height:  2em;
+  }
+</style>
--- app/javascripts/accounts.vue
+++ app/javascripts/accounts.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="accounts">
+    <account-header></account-header>
     These are the accounts
   </div>
 </template>
+
+<script>
+  import AccountHeader from './account_header.vue';
+
+  export default {
+    components: {
+      AccountHeader
+    }
+  }
+</script>
