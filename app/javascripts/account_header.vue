66e97b4176adca92dbb49e56fa6eeb8b5527b5f0
Chris Holtz <choltz@gmail.com>
2017-12-07 08:39:09 -0500

    Added some detail to the account header

--- app/javascripts/account_header.vue
+++ app/javascripts/account_header.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="account-header">
-    All Accounts
+    <div class="title">
+      All Accounts
+    </div>
+    <div>Cleared Balance</div>
+    <div>Uncleared Balance</div>
+    <div>Working Balance</div>
   </div>
 </template>
 
 <style>
   .account-header {
+    display:          flex;
     color:            white;
     background-color: #003540;
-
     padding: .5em;
-    height:  2em;
+  }
+
+  .account-header .title {
+    color: white;
+    background-color: #16A336;
+    display: table-cell;
+    vertical-align: middle;
+    padding: .5em;
+    border-radius: 5px;
+    font-weight: 300;
+    font-size: 1.3em;
   }
 </style>
