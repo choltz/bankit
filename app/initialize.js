a67ae5c8f989d271ca0929414a7b670f5f87a8f9
Chris Holtz <choltz@gmail.com>
2017-12-19 22:15:14 -0500

    Fixed budget/reports route initialization

--- app/initialize.js
+++ app/initialize.js
@@ -13,13 +13,20 @@ Vue.config.productionTip = false;
 axios.get('/api/accounts/active')
   .then((results) =>{
     if (document.location.pathname.match(/\/accounts\/\d+/)) {
-      let accountId = document.location.pathname.match(/\/accounts\/(\d+)/)[1];
+      let accountId      = document.location.pathname.match(/\/accounts\/(\d+)/)[1];
       let currentAccount = results.data.find((account) => { return account.id == parseInt(accountId); });
 
       new Vue({
         el:       '#app',
         render:   h => h(App),
         store: store(results.data, currentAccount),
+        router
+      });
+    } else {
+      new Vue({
+        el:       '#app',
+        render:   h => h(App),
+        store: store(),
         router
       });
     }
