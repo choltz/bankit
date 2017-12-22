e7faf14ec465b1cf5bea5a3efd6053e15b4a3d35
Chris Holtz <choltz@gmail.com>
2017-12-22 17:44:08 -0500

    Started adding links to the accounts actionbar

--- app/javascripts/components/actionbars/accounts_actionbar.vue
+++ app/javascripts/components/actionbars/accounts_actionbar.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>This is the accounts actionbar</div>
+  <div class="accounts-actionbar">
+    <div class="accounts-actionbar-item">Add a transaction</div>
+    <div class="accounts-actionbar-item">Edit</div>
+    <div class="accounts-actionbar-item">Import</div>
+    <div class="accounts-actionbar-item">Undo</div>
+    <div class="accounts-actionbar-item">Redo</div>
+  </div>
 </template>
+
+<style>
+  .accounts-actionbar {
+    display: flex;
+    font-size: .8em;
+  }
+
+  .accounts-actionbar-item {
+    padding: 0 .5em;
+    color:   #009cc2;
+  }
+</style>
