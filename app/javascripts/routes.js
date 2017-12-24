225662aa3305443616bfc19fd97ffc44129511b6
Chris Holtz <choltz@gmail.com>
2017-12-24 09:57:36 -0500

    Renamed Accounts component to Account

--- app/javascripts/routes.js
+++ app/javascripts/routes.js
@@ -2,7 +2,7 @@ import Vue       from 'vue';
 import VueRouter from 'vue-router';
 import Budget    from 'javascripts/components/budget.vue';
 import Reports   from 'javascripts/components/reports.vue';
-import Accounts  from 'javascripts/components/accounts.vue';
+import Account   from 'javascripts/components/account.vue';
 
 Vue.use(VueRouter);
 
@@ -10,8 +10,8 @@ let routes = [
   { path: '/',             component: Budget },
   { path: '/budget',       component: Budget },
   { path: '/reports',      component: Reports },
-  { path: '/accounts',     component: Accounts },
-  { path: '/accounts/:id', component: Accounts }
+  { path: '/accounts',     component: Account },
+  { path: '/accounts/:id', component: Account }
 ];
 
 export default new VueRouter({
