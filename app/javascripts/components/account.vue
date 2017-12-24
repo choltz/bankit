68a768a6bedadd21d1f17be0adeb1a7fe15c5f8e
Chris Holtz <choltz@gmail.com>
2017-12-24 14:16:36 -0500

    Added transaction grid to the account view

--- app/javascripts/components/account.vue
+++ app/javascripts/components/account.vue
@@ -2,7 +2,30 @@
   <div class="accounts">
     <account-header></account-header>
     <action-bar></action-bar>
-    {{transactions}}
+    <table>
+      <tr>
+        <th></th>
+        <th></th>
+        <th>Date</th>
+        <th>Payee</th>
+        <th>Category</th>
+        <th>Memo</th>
+        <th>Outflow</th>
+        <th>Inflow</th>
+        <th></th>
+      </tr>
+      <tr v-for="transaction in transactions">
+        <td></td>
+        <td></td>
+        <td>{{transaction.transaction_at}}</td>
+        <td>{{transaction.payee}}</td>
+        <td>{{transaction.category}}</td>
+        <td>{{transaction.memo}}</td>
+        <td>{{transaction.outflow}}</td>
+        <td>{{transaction.inflow}}</td>
+        <td></td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -34,3 +57,19 @@
     }
   }
 </script>
+
+<style>
+  .accounts table {
+    width: 100%;
+    border-spacing: 0px;
+  }
+
+  .accounts th,
+  .accounts td {
+    font-weight: normal;
+    font-size:   .8em;
+    border-bottom:      1px solid #dee3e8;
+    border-left:      1px solid #dee3e8;
+    padding:    .5em;
+  }
+</style>
