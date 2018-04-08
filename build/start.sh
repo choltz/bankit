9749a94e7f5fd966509f201bf16d146c4e7522c0
Chris Holtz <choltz@gmail.com>
2018-04-07 21:17:20 -0400

    Moved permission setting code to start script.

--- build/start.sh
+++ build/start.sh
@@ -3,6 +3,8 @@
 
 # get and import database backup
 
+chown -R web:web /home/web/bankit/log/
+chown -R web:web /home/web/bankit/db/
 
 /bin/su - web -c "export PATH=\"$PATH:/home/web/bin:/home/web/apps/ruby/bin:/home/web/app/node_modules/.bin\" &&\
                   export SECRET_KEY_BASE=64743aa516abe1515a648c793aded42023e6df199999ea4e3acac77f229fd5733cf248970323d82ae33734180b907d629a5030ea2679e383c3992571d0bb3ef1 &&\
